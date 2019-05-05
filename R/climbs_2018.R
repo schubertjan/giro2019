46c37bae84c4ba9d912ee3f57911a38321e63224
schuberj <jan.schubert@kantar.com>
2019-05-05 19:40:28 +0200

    added 2017 and 2018 climb data

--- R/climbs_2018.R
+++ R/climbs_2018.R
@@ -1,12 +1,13 @@
-#' Categorized climbs from giro 2017
+#' Categorized climbs from giro 2018
 #'
-#' A dataset containing categorized climbs from giro 2017.
+#' A dataset containing categorized climbs from giro 2018.
 #'
 #' @format A data frame with 39 rows and 13 variables:
 #' \describe{
 #'   \item{stage}{stage, character}
 #'   \item{start.finish}{start and finish town, character}
 #'   \item{kom}{name of the climb, character}
+#'   \item{cat}{category climb, factor}
 #'   \item{alt.finish}{altitude at finish of the climb, number}
 #'   \item{dist.finish}{distance from finish, number}
 #'   \item{start}{start town of a climb, factor}
@@ -16,7 +17,6 @@
 #'   \item{disl.m}{elevation difference, number}
 #'   \item{p.media}{average gradient, number}
 #'   \item{p.max}{max gradient, number}
-#'   \item{cat}{category climb, factor}
 #' }
-#' @source \url{https://static2.giroditalia.it/wp-content/uploads/2017/04/Garibaldi_2017.pdf}
-"climbs_2017"
+#' @source \url{hhttps://legaciclismo.files.wordpress.com/2018/05/garibaldi-2018.pdf}
+"climbs_2018"

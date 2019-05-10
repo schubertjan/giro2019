edd153e6c071967b1c383d69fad5c0439d294b5e
schuberj <jan.schubert@kantar.com>
2019-05-10 18:24:31 +0200

    may 11

---
+++ R/tt.R
@@ -0,0 +1,14 @@
+#' Time Trial Data
+#'
+#' This dataset contains tt data form years 2019 and 2018
+#'
+#' @format A data frame with 21 rows and 5 variables:
+#' \describe{
+#'   \item{rider}{rider's name, character}
+#'   \item{race}{name of the race, character}
+#'   \item{time}{time in minutes, numeric}
+#'   \item{speed}{speed in kmh, numeric}
+#'   \item{len}{lenght in km, numeric}
+#' }
+#' @source \url{http://procyclingstats.com/}
+"tt"
